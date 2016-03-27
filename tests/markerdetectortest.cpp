1550262cbf44632322724f5ea6a989c1ee8bd61f
duburlan <alien-box@yandex.ru>
2016-03-27 11:36:10 +0300

    improved test

--- tests/markerdetectortest.cpp
+++ tests/markerdetectortest.cpp
@@ -87,18 +87,19 @@ void MarkerDetectorTest::distortUndistort()
 {
     PointArraySp distPoints;
     PointArraySp udistPoints, udistPoints2;
-
     CameraData camData;
-    double fx = 4. * 800. / 8.; // focal length * image width / sensor width
-    double fy = 4. * 600. / 6.;
-    double cx = 800. / 2.;
-    double cy = 600. / 2.;
+
+    double fx = 1000.;
+    double fy = 950.;
+    double cx = 324.;
+    double cy = 249.;
     camData.cameraMatrix = (cv::Mat_<double>(3, 3) <<
                             fx, 0., cx,
                             0., fy, cy,
                             0., 0., 1.);
 
-    camData.distCoefs = vector<double>{0.1, 0.01, 0.1, 0.01};
+    // k1, k2, p1, p2, k3
+    camData.distCoefs = vector<double>{0.5, -0.5, 0.07, -0.05, 0.000005};
 
     udistPoints.push_back(Point2d{cx, cy});
     udistPoints.push_back(Point2d{0., 0.});
@@ -107,7 +108,9 @@ void MarkerDetectorTest::distortUndistort()
     undistortPoints(distPoints, udistPoints2, camData);
 
     QCOMPARE(udistPoints.size(), udistPoints2.size());
-    QVERIFY(udistPoints[0] == udistPoints2[0]);
-    QVERIFY(qAbs(udistPoints[1].x - udistPoints2[1].x) < 0.1);
-    QVERIFY(qAbs(udistPoints[1].y - udistPoints2[1].y) < 0.1);
+    for (int i {0}; i < (int)udistPoints.size(); ++i) {
+        Point2d residual = udistPoints[i] - udistPoints2[i];
+        double norm = sqrt(residual.dot(residual));
+        QVERIFY(norm < 2e-3);
+    }
 }
